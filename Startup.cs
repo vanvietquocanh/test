c761b3709edddfa5cbac94c9fbf6c9c16c31341a
unknown <PC-Nokia@DESKTOP-LO97IC6.kuikdelivery.com>
2018-04-09 15:56:50 +0700

    this is comment

--- Startup.cs
+++ Startup.cs
@@ -23,15 +23,43 @@ namespace OfferTest
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddAuthentication(options =>
+            {
+
+                options.DefaultScheme = "bearer"; // this is the default scheme to be used
+                options.DefaultChallengeScheme = "bearer";
+            })
+
+         .AddJwtBearer("bearer", options =>
+         {
+             options.Authority = "https://localhost:5000"; // URL of Identity Server; use IConfiguration instead of hardcoding 
+
+
+               options.RequireHttpsMetadata = true; // require HTTPS (may be disabled in development, but advice against it)
+               options.SaveToken = false; // cache the token for faster authentication
+               options.IncludeErrorDetails = true; // get more details on errors; may be disabled in production 
+           });
             services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
+            app.UseForwardedHeaders();
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                app.UseDatabaseErrorPage();
+                app.UseBrowserLink();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+            if (env.IsProduction() || env.IsStaging())
+            {
+                throw new Exception("Not development.");
             }
 
             app.UseMvc();
